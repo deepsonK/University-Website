577f43d48ec1abd41d1242b40393c667fd5ab36f
AliMahmoudDev <123aliactionx5@gmail.com>
2026-07-14 09:43:18 +0000

    fix: replace deprecated ReactDOM.render with createRoot API
    Closes #16

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Root element not found. Make sure there is a div with id='root' in your HTML.");
+}
+
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
